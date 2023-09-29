f80e5ae8d71666bfee4fc6e047bd62ab63c3f975
Michael Hettiarachchi <dixielofranco2002@yahoo.fr>
2023-09-29 12:16:06 +0200

    listes de premières fonctions
    j'ai listé les nom des fonctions primaires pour convertir les différentes bases.

---
+++ projet 1.py
@@ -0,0 +1,18 @@
+def convert_hex_to_dec(htd):
+    
+    
+def convert_dec_to_hex(dth):
+    
+
+def convert_hex_to_bin(htb):
+    
+    
+def convert_bin_to_hex(bth):
+    
+    
+def convert_dec_to_bin(dtb):
+    
+    
+def convert_bin_to_dec(btd):
+    
+    
