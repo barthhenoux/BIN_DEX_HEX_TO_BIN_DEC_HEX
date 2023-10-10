b880dc375524d51d74a6954252dd41302cd5bcfd
Michael Hettiarachchi <dixielofranco2002@yahoo.fr>
2023-10-10 09:19:03 +0200

    dec to hex
    il reste a regler le problème du message d'erreur, le code est a vérifier car resultat incohérent

--- convert_dec_to_hex.py
+++ convert_dec_to_hex.py
@@ -12,13 +12,38 @@ def convert_dec_to_hex(dth):
 
     return hexadecimal
 
-# Demander à l'utilisateur d'entrer un nombre décimal
+# Demander à l'utilisateur d'entrer un nombre décimal 
 decimal_input = int(input("Entrez un nombre décimal que vous voulez convertir en base hexadecimal: "))
 
-while decimal_input==type(str):
-    print("Veuillez entrez un nombre décimal")
+# Initialisation de la variable decimal_input_str
+decimal_input_str = ""
+
+# Tant que l'entrée n'est pas un nombre décimal valide, continuez à demander à l'utilisateur
+while True:
+    decimal_input_str = input("Entrez un nombre décimal que vous voulez convertir en base hexadecimal: ")
+    
+    # Vérifier si l'entrée est une chaîne de caractères (str)
+    if isinstance(decimal_input_str, int):
+        print("Veuillez entrer un nombre décimal valide.")
+    else:
+        try:
+            decimal_input = int(decimal_input_str)
+            break  # Sortir de la boucle si l'entrée est valide
+        except ValueError:
+            print("Entrée non valide. Assurez-vous d'entrer un nombre décimal valide.")
+
+# À ce stade, decimal_input contient un nombre décimal valide
+# Appeler la fonction de conversion et afficher le résultat
+hexadecimal_result = convert_dec_to_hex(decimal_input)
+print(f"Le nombre hexadécimal correspondant est : {hexadecimal_result}")
 
     
 # Appeler la fonction de conversion et afficher le résultat
 hexadecimal_result = convert_dec_to_hex(decimal_input)
 print(f"Le nombre hexadécimal correspondant est : " +str(hexadecimal_result))
+
+
+
+
+
+ 
