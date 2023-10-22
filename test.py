935f51ec015ccce0de668020901446d67da26261
Michael Hettiarachchi <dixielofranco2002@yahoo.fr>
2023-10-22 14:05:20 +0200

    Update test.py
    on va remlacer dec to bin par ce code 5fois plus simple

--- test.py
+++ test.py
@@ -1,15 +1,19 @@
-def dec_to_bin(dtb):
-    
-    nombre=int(input("Entrez un nombre decimal que vous sohaitez convertir en binaire: "))
-    nombre_decimal=nombre
+def dec_to_bin():
+    nombre = int(input("Entrez un nombre décimal que vous souhaitez convertir en binaire: "))
+    nombre_decimal = nombre
+    binaire = []  # Créez une liste pour stocker les bits binaires
 
-    if nombre==0:
+    if nombre == 0:
         print("Le nombre 0 en base 10 équivaut à 0 en base 2")
     else:
-        while nombre!=0:
-            quotient=nombre/2
-            reste=nombre%2
-            print(reste)
-            nombre=quotient
+        while nombre != 0:
+            quotient = nombre // 2
+            reste = nombre % 2
+            binaire.insert(0, str(reste))  # Ajoutez le bit au début de la liste sous forme de chaîne
+            nombre = quotient
 
-    print("Le nombre",nombre_decimal,"en base 10 correspond à la suite de 0 et de 1 préceente notée du bas vers le haut")
+    binaire_str = ''.join(binaire)  # Convertir la liste en une seule chaîne
+    print(f"Le nombre {nombre_decimal} en base 10 correspond à {binaire_str} en base 2.")
+
+# Appeler la fonction pour effectuer la conversion
+dec_to_bin()
